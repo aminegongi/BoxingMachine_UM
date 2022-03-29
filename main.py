1a18fde69c5a89acbb61786578f2bdad6dc3992a
aminegongi <amine.gongi@esprit.tn>
2022-03-29 21:08:16 +0100

    py

---
+++ main.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication
+import sys
+import window
+
+class ExampleApp(QtWidgets.QMainWindow, window.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(ExampleApp, self).__init__(parent)
+        self.setupUi(self)
+
+def main():
+    app = QApplication(sys.argv)
+    form = ExampleApp()
+    form.show()
+    form.showFullScreen()
+    app.exec_()
+    
+
+if __name__ == '__main__':
+    main()
